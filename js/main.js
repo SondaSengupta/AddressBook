9ddacb7d0200cf697f64c520f13b746dd3ddf056
SondaSengupta <sondasengupta@gmail.com>
2014-12-05 12:21:33 -0600

    Info shows up in table`

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
   angular.module('AddressBook',[])
     .controller('AddressBookController',function(){
       var vm = this;
-      vm.tasks = [
+      vm.contact = [
       {
         name: 'Ron Swanson',
         phone: '123-456-7890',
         email: 'rswanson@park.com',
-        dates: 'Wedding: First Episode',
+        dates: 'Wedding',
         group: 'Family',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Nick_Offerman_at_UMBC_%28cropped%29.jpg/640px-Nick_Offerman_at_UMBC_%28cropped%29.jpg'
       },
